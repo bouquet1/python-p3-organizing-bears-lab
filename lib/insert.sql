ae0b60b8c732be8d229a7789cdf75aa9bb286ef0
bouquet1 <bukuleta@gmail.com>
2023-08-12 18:38:26 -0700

    insert into passed

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,9 @@
-INSERT 
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES  (1, 'Mr. Chocolate', 2, 'M', 'Dark Brown', 'social', True),
+        (2, 'Rowdy', 11, 'M', 'Light Brown', 'social', False),
+        (3, 'Tabitha', 5, 'F', 'Ashy Black', 'shy', True),
+        (4, 'Sergeant Brown', 9, 'M', 'Mocha Brown', 'hyper', True),
+        (5, 'Melissa', 6, 'F', 'Gingerbread', 'people-oriented', True),
+        (6, 'Grinch', 5, 'M', 'Charcoal', 'agressive', True),
+        (7, 'Wendy', 11, 'F', 'Light Brown', 'shy', True),
+        (8, NULL, 1, 'F', 'Umber', 'timid', True);
